2539757246a00e6e98d3d08e1ab5425d484e4f06
Dave <git@freakern.de>
2024-01-04 22:14:46 +0100

    added missing lib folder

---
+++ lib/argparselib.py
@@ -0,0 +1,95 @@
+import argparse
+import os
+
+
+def get_args():
+    ap = argparse.ArgumentParser(prog="POLESTAR2MQTT", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    global_group = ap.add_argument_group("Global")
+    global_group.add_argument('-d',
+                              action='store_true',
+                              default=os.environ.get(key='POLESTAR2MQTT_DEBUG') or False,
+                              dest='debug',
+                              help='Enable Debug Mode'
+                              )
+    global_group.add_argument('-v',
+                              action='store_true',
+                              default=os.environ.get(key='POLESTAR2MQTT_VERBOSE') or False,
+                              dest='verbose',
+                              help='(Verbose) Talk to me baby'
+                              )
+
+    polestar_group = ap.add_argument_group("Polestar")
+    polestar_group.add_argument('--polestar-email',
+                              default=os.environ.get(key='POLESTAR2MQTT_POLESTAR_EMAIL') or argparse.SUPPRESS,
+                              dest='polestar_email',
+                              help='Polestar email',
+                              required=not os.environ.get(key='POLESTAR2MQTT_POLESTAR_EMAIL'),
+                              )
+    polestar_group.add_argument('--polestar-password',
+                              default=os.environ.get(key='POLESTAR2MQTT_POLESTAR_PASSWORD') or argparse.SUPPRESS,
+                              dest='polestar_password',
+                              help='Polestar password',
+                              required=not os.environ.get(key='POLESTAR2MQTT_POLESTAR_PASSWORD')
+                              )
+    polestar_group.add_argument('-i', '--request-interval',
+                              default=os.environ.get(key='POLESTAR2MQTT_REQUEST_INTERVAL') or 60,
+                              dest='request_interval',
+                              help='The request interval',
+                              type=int
+                              )
+
+    mqtt_group = ap.add_argument_group("MQTT", "Information needed for the MQTT Connection")
+    mqtt_group.add_argument('-s', '--mqtt-single',
+                            action='store_true',
+                            default=os.environ.get(key='POLESTAR2MQTT_MQTT_SINGLE') or False,
+                            dest='mqtt_single',
+                            help='Send each value individually to the MQTT Broker'
+                            )
+    mqtt_group.add_argument('--mqtt-single-separator',
+                            default=os.environ.get(key='POLESTAR2MQTT_MQTT_SINGLE_SEPARATOR') or "/",
+                            dest='mqtt_single_separator',
+                            help='Separator for the single (-s) parameter. If it is a slash, you\'ll be able to subscripe just to subtopics'
+                            )
+    mqtt_group.add_argument('-c', '--cache',
+                            action='store_true',
+                            default=os.environ.get(key='POLESTAR2MQTT_MQTT_CACHE') or False,
+                            dest='mqtt_cache',
+                            help='Cache the send values (works best in single (-s) mode)'
+                            )
+    mqtt_group.add_argument('--mqtt-host',
+                            default=os.environ.get(key='POLESTAR2MQTT_MQTT_HOST') or 'localhost',
+                            dest='mqtt_host',
+                            help='MQTT Host to connect to'
+                            )
+    mqtt_group.add_argument('--mqtt-port',
+                            default=os.environ.get(key='POLESTAR2MQTT_MQTT_PORT') or '1883',
+                            dest='mqtt_port',
+                            help='MQTT port to connect to',
+                            type=int
+                            )
+    mqtt_group.add_argument('--mqtt-topic',
+                            default=os.environ.get(key='POLESTAR2MQTT_MQTT_TOPIC') or 'polestar2mqtt',
+                            dest='mqtt_topic',
+                            help='MQTT Topic to publish to',
+                            required=False)
+    mqtt_group.add_argument('--mqtt-client-id',
+                            default=os.environ.get(key='POLESTAR2MQTT_MQTT_CLIENT_ID') or 'polestar2mqtt',
+                            dest='mqtt_client_id',
+                            help='MQTT Client-ID',
+                            required=False
+                            )
+    mqtt_group.add_argument('--mqtt-user',
+                            default=os.environ.get(key='POLESTAR2MQTT_MQTT_USER') or None,
+                            dest='mqtt_user',
+                            help='MQTT user to connect to',
+                            required=False,
+                            )
+    mqtt_group.add_argument('--mqtt-password',
+                            default=os.environ.get(key='POLESTAR2MQTT_MQTT_PASS') or None,
+                            dest='mqtt_password',
+                            help='MQTT password to connect to',
+                            required=False,
+                            )
+
+    args = ap.parse_args()
+    return args
---
+++ lib/mqtt_helper.py
@@ -0,0 +1,90 @@
+import json
+import logging
+import time
+
+from collections.abc import MutableMapping
+from paho.mqtt import client as mqtt_client
+
+
+_LOGGER = logging.getLogger(__name__)
+
+class mqtt_helper:
+    def __init__(self):
+        self.first_reconnect_delay = 1
+        self.reconnect_rate = 2
+        self.max_reconnect_count = 12
+        self.max_reconnect_delay = 60
+        self.mqttclient = None
+        self.mqttcache = {}
+
+    def connect_mqtt(self, broker, port, client_id, username=None, password=None):
+
+        def on_connect(self, client, userdata, flags, rc):
+            if rc == 0:
+                print("Connected to MQTT Broker!")
+            else:
+                print("Failed to connect, return code %d\n", rc)
+
+        def on_disconnect(self, client, userdata, rc):
+            _LOGGER.info("Disconnected with result code: %s", rc)
+            reconnect_count, reconnect_delay = 0, self.first_reconnect_delay
+            while reconnect_count < self.max_reconnect_count:
+                _LOGGER.info("Reconnecting in %d seconds...", reconnect_delay)
+                time.sleep(reconnect_delay)
+
+                try:
+                    client.reconnect()
+                    _LOGGER.info("Reconnected successfully!")
+                    return
+                except Exception as err:
+                    _LOGGER.error("%s. Reconnect failed. Retrying...", err)
+
+                reconnect_delay *= self.reconnect_rate
+                reconnect_delay = min(reconnect_delay, self.max_reconnect_delay)
+                reconnect_count += 1
+            _LOGGER.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+
+        # Set Connecting Client ID
+        self.mqttclient = mqtt_client.Client(client_id)
+        self.mqttclient.username_pw_set(username, password)
+        self.mqttclient.on_connect = on_connect
+        self.mqttclient.on_disconnect = on_disconnect
+        self.mqttclient.connect(broker, port)
+
+    def flatten(self, d: MutableMapping, parent_key: str = '', sep: str = '/') -> MutableMapping:
+        items = []
+        if isinstance(d, MutableMapping):
+            for k, v in d.items():
+                new_key = parent_key + sep + k if parent_key else k
+                if isinstance(v, (MutableMapping, list)):
+                    items.extend(self.flatten(v, new_key, sep=sep).items())
+                else:
+                    items.append((new_key, v))
+        elif isinstance(d, list):
+            index = 0
+            for v in d:
+                new_key = parent_key + sep + str(index) if parent_key else str(index)
+                if isinstance(v, (MutableMapping, list)):
+                    items.extend(self.flatten(v, new_key, sep=sep).items())
+                else:
+                    items.append((new_key, v))
+        return dict(items)
+
+
+    def send_data(self, base_topic, data_to_send, mqtt_cache, mqtt_single, mqtt_single_separator):
+        
+        _LOGGER.debug("Cache %s", self.mqttcache)
+        if mqtt_single:
+            data_to_send = self.flatten(data_to_send, sep=mqtt_single_separator)
+            for k, v in data_to_send.items():
+                topic_to_use = base_topic + "/" + str(k)
+                if not mqtt_cache or (mqtt_cache and (k not in self.mqttcache or self.mqttcache[k] != str(v))):
+                    value_to_use = str(v)
+                    _LOGGER.info("Send %s -> %s", topic_to_use, value_to_use)
+                    self.mqttclient.publish(topic_to_use, value_to_use)
+                    self.mqttcache[k] = value_to_use
+                else:
+                    _LOGGER.debug("Found %s in cache, skip!", topic_to_use)
+        else:
+            _LOGGER.debug(data_to_send)
+            self.mqttclient.publish(base_topic, json.dumps(data_to_send))
